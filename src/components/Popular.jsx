f91feabeec58e122060a52c018f08a204a752236
Deirdre Sullivan <dsulliv4@binghamton.edu>
2022-05-06 23:53:42 -0400

    pages

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,10 @@
 function Popular() {
+
+  const getPopular = async ()=> {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+  }
+
   return  <div>Popular</div>
 }
 
-export default Popular
+export default Popular;

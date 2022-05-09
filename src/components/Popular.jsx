a97b77befa4eb2b028d731097478609d8580e3f1
Deirdre Sullivan <dsulliv4@binghamton.edu>
2022-05-08 23:34:01 -0400

    useEffect hook

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+
 function Popular() {
+
+  useEffect(() => {
+    getPopular();
+  },[]);
 
   const getPopular = async ()=> {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+    const data = await api.json();
   }
 
   return  <div>Popular</div>
